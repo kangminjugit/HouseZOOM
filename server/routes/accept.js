340a0dc0341198d628624d8fd1cc450a13707dcb
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2022-04-28 02:32:19 +0900

    Accept, deny student api

--- server/routes/accept.js
+++ server/routes/accept.js
@@ -1,6 +1,7 @@
 var express = require('express');
 const pool = require('../db/config');
 var router = express.Router();
+const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
 
 /**
  * @swagger
@@ -45,7 +46,7 @@ var router = express.Router();
  *                          type: object
  */
 
- router.get(`/`,async (req, res, next) => {
+ router.get(`/`,teacherAuthMiddleware, async (req, res, next) => {
     const {classId, teacherId} = req.query;
 
     // classId, teacherId 중 하나라도 없으면 에러
@@ -75,6 +76,142 @@ var router = express.Router();
         });  
     }catch(error){
         next(error);
+    }finally{
+        connection.release();
+    }
+});
+
+/**
+ * @swagger
+ *  /api/apply/accept:
+ *    post:
+ *      tags: [Accept appliance]
+ *      summary: 학생 승인 (선생님 아이디 토큰 필요)
+ *      produces:
+ *      - "application/json; charset=utf-8"
+ *      parameters:
+ *         - in: body
+ *           name: studentId
+ *           description:  승인할 학생 아이디
+ *           schema:
+ *              type: object
+ *              required:
+ *                  - studentId
+ *              properties:
+ *                  studentId:
+ *                      type: string
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ *          '400':
+ *              description: classId, teacherId, studentId 중 하나라도 없으면 에러
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ */
+
+ router.post(`/accept`, teacherAuthMiddleware, async (req, res, next) => {
+    const {studentId} = req.body;
+
+    // studentId 없으면 에러
+    if(studentId===''){
+        let error = new Error('classId, teacherId, studentId are required!');
+        error.status = 400;
+        next(error);
+        return;       
+    }
+
+    const connection = await pool.getConnection(async conn => conn);
+    try{
+        await connection.query(
+            'update student set isAccepted = ? where id = ?', [
+                'ACCEPT', studentId
+            ]
+        );
+
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        res.send({
+            "status": 'success',
+            "code": 200,
+            "data": {},
+            "message": `${studentId}를 정상적으로 승인했습니다.`
+        }); 
+    }catch(err){
+        next(err);
+    }finally{
+        connection.release();
+    }
+});
+
+
+
+/**
+ * @swagger
+ *  /api/apply/deny:
+ *    post:
+ *      tags: [Accept appliance]
+ *      summary: 학생 거절 (선생님 아이디 토큰 필요)
+ *      produces:
+ *      - "application/json; charset=utf-8"
+ *      parameters:
+ *         - in: body
+ *           name: studentId
+ *           description:  거절할 학생 아이디
+ *           schema:
+ *              type: object
+ *              required:
+ *                  - studentId
+ *              properties:
+ *                  studentId:
+ *                      type: string
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ *          '400':
+ *              description: classId, teacherId, studentId 중 하나라도 없으면 에러
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ */
+
+ router.post(`/deny`, teacherAuthMiddleware, async (req, res, next) => {
+    const {studentId} = req.body;
+
+    // studentId 없으면 에러
+    if(studentId===''){
+        let error = new Error('classId, teacherId, studentId are required!');
+        error.status = 400;
+        next(error);
+        return;       
+    }
+
+    const connection = await pool.getConnection(async conn => conn);
+    try{
+        await connection.query(
+            'update student set isAccepted = ? where id = ?', [
+                'DENY', studentId
+            ]
+        );
+
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        res.send({
+            "status": 'success',
+            "code": 200,
+            "data": {},
+            "message": `${studentId}를 정상적으로 거절했습니다.`
+        }); 
+    }catch(err){
+        next(err);
     }finally{
         connection.release();
     }
