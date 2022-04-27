f285df967bdeb2a61d5caf9de568169dfe247cef
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2022-04-28 04:02:37 +0900

    Get current avatar item api

---
+++ server/routes/avatar.js
@@ -0,0 +1,90 @@
+var express = require('express');
+const pool = require('../db/config');
+var router = express.Router();
+const {studentAuthMiddleware} = require('../middlewares/authmiddleware');
+
+/**
+ * @swagger
+ * tags:
+ *  name: Avatar
+ *  description: 캐릭터 꾸미기 관련 API
+ */
+
+/**
+ * @swagger
+ *  /api/avatar/default:
+ *    get:
+ *      tags: [Avatar]
+ *      summary: 기본 캐릭터 몸통 
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "image/png":
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              image:
+ *                                  type: image
+ *                                  description: 기본 캐릭터
+ */
+ router.get(`/default`, async (req, res, next) => {
+    
+    res.set({ 'content-type': 'image/png' });
+    res.sendFile('/Users/kangminju/Documents/GitHub/HouseZOOM/server/image/avatar_body.PNG');
+});
+
+/**
+ * @swagger
+ *  /api/avatar/cur-item?studentId=#:
+ *    get:
+ *      tags: [Avatar]
+ *      summary: 학생의 아바타가 현재 입고있는 옷 반환
+ *      produces:
+ *      - "application/json; charset=utf-8"
+ *      parameters:
+ *          - in: query
+ *            name: studentId
+ *            schema:
+ *              type: string
+ *            required: true
+ *            description: 학생 아이디
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ */
+
+ router.get(`/cur-item`, async (req, res, next) => {
+    const {studentId} = req.query;
+
+    if(studentId === ''){
+        const error = new Error('student id is required!');
+        error.status = 400;
+        next(error);
+        return;
+    }   
+
+    const connection = await pool.getConnection(async conn => conn);
+    try{
+        [rows, fields] = await connection.query('select item.id, item.name, item.type, item.image from my_item,item where my_item.item_id=item.id and my_item.is_cur=1 and my_item.student_id=?', [studentId]);
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        res.send({
+            "status": 'success',
+            "code": 200,
+            "data": {
+                "items": rows
+            },
+            "message": null
+        });
+    }catch(error){
+        next(error);
+    }finally{
+        connection.release();
+    }
+});
+
+module.exports = router;
