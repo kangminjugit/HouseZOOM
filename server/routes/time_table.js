7710c503a95428eb0a0208475432c24559b324ea
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2022-04-28 01:12:12 +0900

    Delete timetable api

--- server/routes/time_table.js
+++ server/routes/time_table.js
@@ -106,11 +106,15 @@ const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
  *          type: object
  *          properties:
  *              day:
+ *                  required: true
  *                  type: string
  *              period:
+ *                  required: true
  *                  type: integer
  *              subject:
- *                  type: string          
+ *                  type: string  
+ *              zoom_url:
+ *                  type: string        
  */
  router.post(`/`,teacherAuthMiddleware, async (req, res, next) => {
     const {classId, timeTable} = req.body;
@@ -133,11 +137,12 @@ const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
 
         try{
             await timeTable.forEach(async (elem) => {
-                if(elem.day === undefined || elem.period === undefined || elem.subject === undefined){
+                if(elem.day === undefined || elem.period === undefined || elem.subject === undefined || elem.zoom_url === undefined){
                     let error = new Error('잘못된 json형식입니다!');
                     error.status = 404;
                     throw error;
                 }
+
                 var [isExist] = await connection.query('select * from time_table where day = ? and period = ?', [
                     elem.day, elem.period
                 ]);
@@ -149,8 +154,8 @@ const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
                     ]);
                 }
                 else
-                    await connection.query('insert into time_table(day, period, subject, class_id) values(?, ?, ?, ?)', [
-                        elem.day, elem.period, elem.subject, classId
+                    await connection.query('insert into time_table(day, period, subject, class_id, zoom_url) values(?, ?, ?, ?, ?)', [
+                        elem.day, elem.period, elem.subject, classId, elem.zoom_url
                     ]);
 
                 res.set({ 'content-type': 'application/json; charset=utf-8' });
@@ -171,6 +176,76 @@ const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
         connection.release();
     }
 });
+
+
+/**
+ * @swagger
+ *  /api/time-table:
+ *    delete:
+ *      tags: [Time table]
+ *      summary: 해당 반의 전체 시간표 삭제(선생님 토큰 필요!) 
+ *      consumes:
+ *          - application/json; charset=utf-8
+ *      parameters:
+ *         - in: body
+ *           name: class
+ *           description: 반 아이디
+ *           schema:
+ *              type: object
+ *              required:
+ *                  - class_id
+ *              properties:
+ *                  class_id:
+ *                      type: integer    
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ *          '400':
+ *              description: classId 없으면 에러
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object      
+ */
+
+ router.delete(`/`, teacherAuthMiddleware, async (req, res, next) => {
+    const {class_id, auth_code} = req.body;
+
+    // school_code, name, auth_code, year 중 하나라도 없으면 에러
+    if(class_id === null || class_id === undefined){
+        const error = new Error('class_id is required!');
+        error.status = 400;
+        next(error);
+        return;       
+    }
+
+    const connection = await pool.getConnection(async conn => conn);
+    try{
+        await connection.query(
+            'DELETE FROM time_table WHERE class_id = ? ',
+            [class_id]);
+
+        
+        // 정상적으로 행 추가 후엔 새로 추가된 반의 id를 response로 보냄
+        res.set({ 'content-type': 'application/json; charset=utf-8' });
+        res.send({
+            "status": 'success',
+            "code": 200,
+            "data": null,
+            "message": '성공적으로 시간표를 삭제하였습니다.'
+        });  
+
+    }catch(error){
+        next(error);
+    }finally{
+        connection.release();
+    }
+});
+
 
 module.exports = router;
 
