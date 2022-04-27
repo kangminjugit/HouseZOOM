39533a4394ede12d7ecbadee18345bbca7efd158
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2022-04-27 20:11:49 +0900

    Update app.js

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ var schoolRouter = require('./routes/school');
 var classRouter = require('./routes/class');
 var checkRouter = require('./routes/check_id');
 var loginRouter = require('./routes/login');
-var avatarRouter = require('./routes/avatar');
+// var avatarRouter = require('./routes/avatar');
 var imageRouter = require('./routes/image');
 var timeTableRouter = require('./routes/time_table');
 
@@ -40,7 +40,7 @@ app.use('/api/school', schoolRouter);
 app.use('/api/class', classRouter);
 app.use('/api/check', checkRouter);
 app.use('/api/login', loginRouter);
-app.use('/api/avatar', avatarRouter);
+// app.use('/api/avatar', avatarRouter);
 app.use('/api/image', imageRouter);
 app.use('/api/time-table',timeTableRouter);
 
