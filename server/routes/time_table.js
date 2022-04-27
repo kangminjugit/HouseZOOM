16b6435b410e585797c34a29854e0bd05d51d0e5
kangminjugit <62755858+kangminjugit@users.noreply.github.com>
2022-04-28 00:15:58 +0900

    Add or Update time table api

--- server/routes/time_table.js
+++ server/routes/time_table.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const pool = require('../db/config');
+const {teacherAuthMiddleware} = require('../middlewares/authmiddleware');
 
 /**
  * @swagger
@@ -65,6 +66,111 @@ const pool = require('../db/config');
     }
 });
 
+
+/**
+ * @swagger
+ *  /api/time-table:
+ *    post:
+ *      tags: [Time table]
+ *      summary: 해당 반에 시간표 정보 추가/업데이트(선생님 계정 토큰 필요)
+ *      consumes:
+ *          - application/json; charset=utf-8
+ *      parameters:
+ *         - name: timeTable
+ *           in: body
+ *           description: 새로 추가/업데이트할 시간표 정보
+ *           schema:
+ *              type: object
+ *              properties:
+ *                  classId:
+ *                      type: integer
+ *                  timeTable:
+ *                      type: array
+ *                      items:
+ *                          $ref: "#/definitions/timeTable"             
+ *      responses:
+ *          '200':
+ *              description: OK
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ *          '400':
+ *              description: classId 없으면 에러
+ *              content:
+ *                  "application/json; charset=utf-8":
+ *                      schema:
+ *                          type: object
+ * definitions:           
+ *      timeTable:
+ *          type: object
+ *          properties:
+ *              day:
+ *                  type: string
+ *              period:
+ *                  type: integer
+ *              subject:
+ *                  type: string          
+ */
+ router.post(`/`,teacherAuthMiddleware, async (req, res, next) => {
+    const {classId, timeTable} = req.body;
+    if(classId === ''){
+        const error = new Error('class id is required!');
+        error.status = 400;
+        next(error);
+        return;
+    }   
+
+    const connection = await pool.getConnection(async conn => conn);
+    try{
+        
+        [rows, fields] = await connection.query('select * from class where id = ?', [classId]);
+        if(rows.length == 0){
+            let error = new Error('존재하지 않는 반입니다!');
+            error.status = 404;
+            throw error;
+        }
+
+        try{
+            await timeTable.forEach(async (elem) => {
+                if(elem.day === undefined || elem.period === undefined || elem.subject === undefined){
+                    let error = new Error('잘못된 json형식입니다!');
+                    error.status = 404;
+                    throw error;
+                }
+                var [isExist] = await connection.query('select * from time_table where day = ? and period = ?', [
+                    elem.day, elem.period
+                ]);
+                
+
+                if(isExist.length > 0){
+                    await connection.query('UPDATE time_table SET subject = ? WHERE id = ?', [
+                        elem.subject, isExist[0].id
+                    ]);
+                }
+                else
+                    await connection.query('insert into time_table(day, period, subject, class_id) values(?, ?, ?, ?)', [
+                        elem.day, elem.period, elem.subject, classId
+                    ]);
+
+                res.set({ 'content-type': 'application/json; charset=utf-8' });
+                res.send({
+                    "status": 'success',
+                    "code": 200,
+                    "data": {},
+                    "message": '시간표가 성공적으로 저장되었습니다!'
+                });
+            })
+        }catch(err){
+            await connection.rollback();
+            throw error;
+        }
+    }catch(error){
+        next(error);
+    }finally{
+        connection.release();
+    }
+});
 
 module.exports = router;
 
